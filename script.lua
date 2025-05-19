cf9082e1531ddcc6acd3404cd89c94b072fa33db
Etchi9 <ademt35000@gmail.com>
2025-05-19 16:48:48 +0200

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 85896571713843 then 
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f5d517c7a5fe691a69c4f0c33c3bc514.lua"))() 
+else
+    warn("[Light v01] -> Game not supported.") 
+end
