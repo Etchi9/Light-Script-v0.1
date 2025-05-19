452089230d8fc29e2d4f4c71eca476ebc2859cec
Etchi9 <ademt35000@gmail.com>
2025-05-19 17:24:03 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 85896571713843 then 
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f5d517c7a5fe691a69c4f0c33c3bc514.lua"))() 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Etchi9/Light-Script-v0.1/main/main-script.lua"))() 
 else
-    warn("[Light v01] -> Game not supported.") 
+    warn("[Light v0.1] -> Game not supported.") 
 end
